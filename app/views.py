f07e1a447f023780a59a50431476007e6fce6531
reomatsuoka <reo199637458@gmail.com>
2020-12-15 13:50:44 +0900

    practice

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import Profile
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by("-id")[0]
+        return render(request, 'app/index.html',{
+            'profile_data': profile_data,
+        })
